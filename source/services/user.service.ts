40f9bb897f06fed0253a11093f633c353c7aa526
xchezzzx <50829021+xchezzzx@users.noreply.github.com>
2022-10-24 19:45:07 +0300

    st5: getEmployee SP

--- source/services/user.service.ts
+++ source/services/user.service.ts
@@ -1,15 +1,34 @@
 import * as _ from "underscore";
-import { Queries } from "../constants";
+import { Queries, StoredProcedures } from "../constants";
 import { entityWithId, systemError, user } from "../entities";
-import { Role, Status } from "../enums";
+import { Gender, Role, Status } from "../enums";
 import { DateHelper } from "../helpers/date.helper";
 import { SqlHelper } from "../helpers/sql.helper";
 import { ErrorService } from "./error.service";
 
+interface localUser
+{
+    id: number,
+    first_name: string,
+    last_name: string,
+    birthdate: Date,
+    is_male?: number,
+    gender?: string,
+    phone: string,
+    store: string,
+    position_id?: number,
+    position_name?: string,
+    login?: string,
+    role_id?: number,
+    role_name?: string
+}
+
 interface IUserService {
-    updateById(user: user, userId: number): Promise<user>;
-    add(user: user, userId: number): Promise<user>;
-    deleteById(id: number, userId: number): Promise<void>;
+    getEmployeeById(id: number): Promise<user>;
+    getEmployeeByIdStoredProcedure(id: number): Promise<user>;
+    updateEmployeeById(user: user, userId: number): Promise<user>;
+    addEmployee(user: user, userId: number): Promise<user>;
+    deleteEmployeeById(id: number, userId: number): Promise<void>;
 }
 
 export class UserService implements IUserService {
@@ -18,7 +37,36 @@ export class UserService implements IUserService {
         private errorService: ErrorService
         ) { }
 
-    public updateById(user: user, userId: number): Promise<user> {
+    public getEmployeeById(id: number): Promise<user>
+    {
+        return new Promise<user>((resolve, reject) =>
+        {
+            SqlHelper.executeQuerySingleResult<localUser>(this.errorService, Queries.GetUserById, id, Status.Active)
+            .then((queryResult: localUser) => {
+                resolve(this.parseLocalUser(queryResult));
+            })
+            .catch((error: systemError) => {
+                reject(error);
+            });
+        });
+    }
+
+    public getEmployeeByIdStoredProcedure(id: number): Promise<user>
+    {
+        return new Promise<user>((resolve, reject) =>
+        {
+            SqlHelper.executeStoredProcedureSingleResult<localUser>(this.errorService, StoredProcedures.GetUserByIdSP, id, Status.Active)
+            .then((queryResult: localUser) => {
+                resolve(this.parseLocalUser(queryResult));
+            })
+            .catch((error: systemError) => {
+                reject(error);
+            });
+        });
+    }
+
+    public updateEmployeeById(user: user, userId: number): Promise<user>
+    {
         return new Promise<user>((resolve, reject) => {
             const updateDate: Date = new Date();
             SqlHelper.executeQueryNoResult(this.errorService, Queries.UpdateUserById, false, user.firstName, user.lastName, DateHelper.dateToString(updateDate), userId, user.id, Status.Active)
@@ -31,7 +79,7 @@ export class UserService implements IUserService {
         });
     }
     
-    public add(user: user, userId: number): Promise<user> {
+    public addEmployee(user: user, userId: number): Promise<user> {
         return new Promise<user>((resolve, reject) => {
             const createDate: string = DateHelper.dateToString(new Date());
             SqlHelper.createNew(this.errorService, Queries.AddUser, user, user.firstName, user.lastName, user.login as string, user.password as string, Role.RegularUser, createDate, createDate, userId, userId, Status.Active)
@@ -44,7 +92,7 @@ export class UserService implements IUserService {
         });
     }
 
-    public deleteById(id: number, userId: number): Promise<void> {
+    public deleteEmployeeById(id: number, userId: number): Promise<void> {
         return new Promise<void>((resolve, reject) => {
             const updateDate: string = DateHelper.dateToString(new Date());
             SqlHelper.executeQueryNoResult(this.errorService, Queries.DeleteUserById, true, updateDate, userId, Status.NotActive, id, Status.Active)
@@ -55,5 +103,24 @@ export class UserService implements IUserService {
                 reject(error);
             });
         });
+    }
+
+    private parseLocalUser(local: localUser): user {
+        return {
+            id: local.id,
+            firstName: local.first_name,
+            lastName: local.last_name,
+            birthdate: local.birthdate,
+            is_male: local.is_male,
+            gender: local.gender,
+            phone: local.phone,
+            store: local.store,
+            position_id: local.position_id,
+            position_name: local.position_name,
+            login: local.login,
+            // password?: string;
+            role_id: local.role_id,
+            role_name: local.role_name
+        };
     }
 }
